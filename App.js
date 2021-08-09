7f81fa6a429f4781c7c729eef0b72ac0c9cb6119
Carlos Jose <jonnathan.torres.m94@gmail.com>
2021-08-09 16:45:14 -0600

    subiendo 1

--- App.js
+++ App.js
@@ -5,8 +5,62 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+       {
+       /**
+        * Crear un contenedor en forma de columna (vertical) con 3 cajas del mismo tamaño todas.
+        */
+       }
+       { /** 
+       <View style={{flexDirection: 'row', flex: 1}}>
+       <View style={{flex: 1}}>
+         <View style={{backgroundColor: '#faf0f3', flex: 1}}></View>
+         <View style={{backgroundColor: '#fefeaa', flex: 1}}></View>
+         <View style={{ backgroundColor: '#77feaa', flex: 1}}></View>
+
+
+
+       </View>
+       </View>
+       */}
+
+       {/**
+        * Crear un contenedor en forma de columna(vertical)
+        * con tres cajas que sean filas(horizontal)
+        * dentro que tengan cada una 3 cajas de 50px
+        */
+       }
+       {/**
+      <View style={{ backgroundColor: 'red'}}>
+
+      <View style={{ flexDirection: "row", flex: 1}}>
+        <View style={{ width: 50, backgroundColor: '#00fafa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fa00fa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fafa000'}}></View>
+      </View>
+
+      <View style={{ flexDirection: "row", flex: 1}}>
+        <View style={{ width: 50, backgroundColor: '#00fafa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fa00fa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fafa000'}}></View>
+      </View>
+
+      <View style={{ flexDirection: "row", flex: 1}}>
+        <View style={{ width: 50, backgroundColor: '#00fafa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fa00fa'}}></View>
+        <View style={{ width: 50, backgroundColor: '#fafa000'}}></View>
+      </View>
+      
+      </View> */}
+      {/**
+       * Crear un contenedor en forma de fila(horizontal)
+       * que tenga dos elementos distanciados por completo de ellos
+       */}
+
+    <View style={{ flexDirection: 'row', backgroundColor: 'blue', flex: 1,height: 50, justifyContent: "space-between"}}>
+     <View style={{ width: 50, height: 50, backgroundColor: "red"}} />
+     <View style={{ width: 50, height: 50, backgroundColor: "green"}} />
+    </View>
     </View>
   );
 }
@@ -14,8 +68,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
